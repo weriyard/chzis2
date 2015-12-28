9b33c365664de56274905db219e56c85c2f54778
WeriYard <adamer.sk@gmail.com>
2015-12-28 22:11:26 +0100

    add fields

--- loader.py
+++ loader.py
@@ -12,8 +12,12 @@ from chzis.school.models import Lesson
 class LessonLoader(bulkloader.Loader):
     def __init__(self):
         bulkloader.Loader.__init__(self, 'Lesson',
-                                   [('number', lambda x: x.decode('utf-8')),
+                                   [
+#                                       ('number', lambda x: x.decode('utf-8')),
                                     ('name', lambda x: x.decode('utf-8')),
+                                    ('reading', lambda x: bool(int(x))),
+                                    ('demo',  lambda x: bool(int(x))),
+                                    ('discourse',  lambda x: bool(int(x)))
 #                                    ('description', str),
 #                                    ('last_modification', lambda x : datetime.now)
                                    ])
